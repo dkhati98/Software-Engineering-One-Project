1fe44ddb11438306ac04150dbb11049de073dc72
HELLCAT25 <michael_z97@hotmail.com>
2019-11-02 22:12:52 -0500

    added emily's code

--- EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Food.java
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Food.java
@@ -1,5 +1,34 @@
 package com.sred.eatright;
 
 public class Food {
+    private Macros macros;
+    private int calories;
+    private String servingSize;
+    private double numOfServings;
 
+    public void setMacros(Macros macros) {
+        this.macros = macros;
+    }
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+    public void setNumOfServings(double numOfServings) {
+        this.numOfServings = numOfServings;
+    }
+    public void setServingSize(String servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public Macros getMacros() {
+        return macros;
+    }
+    public int getCalories() {
+        return calories;
+    }
+    public double getNumOfServings() {
+        return numOfServings;
+    }
+    public String getServingSize() {
+        return servingSize;
+    }
 }
--- EatRightAndroidStudio/app/src/main/java/com/sred/eatright/MainActivity.kt
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sred.eatright
+package com.example.finaleatright
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
