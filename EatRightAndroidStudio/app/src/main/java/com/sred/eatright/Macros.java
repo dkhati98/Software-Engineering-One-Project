4e3232deaad4d8d39196b69a89e5969932344ef7
HELLCAT25 <michael_z97@hotmail.com>
2019-11-02 22:50:59 -0500

    Revert "added emily's code"
    This reverts commit 1fe44ddb11438306ac04150dbb11049de073dc72.

--- EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Macros.java
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Macros.java
@@ -1,45 +1,5 @@
 package com.sred.eatright;
 
 public class Macros {
-    private double protein;
-    private double carbonHydrates;
-    private double fat;
 
-    private Macros(MacrosBuilder builder) {
-        this.protein = builder.protein;
-        this.carbonHydrates = builder.carbonHydrates;
-        this.fat = builder.fat;
-    }
-
-    public double getProtein() {
-        return protein;
-    }
-    public double getCarbonHydrates() {
-        return carbonHydrates;
-    }
-    public double getFat() {
-        return fat;
-    }
-
-    public static class MacrosBuilder {
-        private double protein;
-        private double carbonHydrates;
-        private double fat;
-
-        public MacrosBuilder setProtein(double protein) {
-            this.protein = protein;
-            return this;
-        }
-        public MacrosBuilder setCarbonHydrates(double carbonHydrates) {
-            this.carbonHydrates = carbonHydrates;
-            return this;
-        }
-        public MacrosBuilder setFat(double fat) {
-            this.fat = fat;
-            return this;
-        }
-        public Macros build() {
-            return new Macros(this);
-        }
-    }
 }
--- EatRightAndroidStudio/app/src/main/java/com/sred/eatright/MainActivity.kt
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.finaleatright
+package com.sred.eatright
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 }
