a687e7e4bd459985d1dba7d0815a71d03177eb4a
Emily Ava <53125759+emilyava0804@users.noreply.github.com>
2019-11-17 13:21:15 -0600

    Create Birthday.java

---
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Birthday.java
@@ -0,0 +1,40 @@
+package com.sred.eatright;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.DatePicker;
+
+
+public class Birthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthday);
+
+        DatePicker datePicker = (DatePicker)findViewById(R.id.datepicker);
+        datePicker.setSpinnersShown(false);
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+        final Button nextButton = (Button) findViewById(R.id.button_next);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (data > 0) {
+                    // Insert code to save to DB //
+                    Intent moveToGetgoal = new Intent(Birthday.this, GetGoal.class);
+                    startActivity(moveToGetgoal);
+                }
+                else {
+                    Toast.makeText(Birthday.this, "Select your birthday", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
