2f4de4b3268b1f711cd7ad02a43441faf29c0d99
Emily Ava <53125759+emilyava0804@users.noreply.github.com>
2019-11-17 13:26:30 -0600

    Update WelcomeActivity.java

--- EatRightAndroidStudio/app/src/main/java/com/sred/eatright/WelcomeActivity.java
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.sred.eatright;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-    }
+
+        final Button nextButton = (Button) findViewById(R.id.button_next);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moveToGender = new Intent(WelcomeActivity.this, Gender.class);
+                startActivity(moveToGender);
+            }
+        })
+    ;}
 }
