3c3423185948ba979f7c1081173f5b46be327bb6
emilyava0804 <emily.cole0804@gmail.com>
2019-11-29 16:28:11 -0600

    added new classes, needing databinding for foods and calories calculated

---
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.sred.eatright;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }}
