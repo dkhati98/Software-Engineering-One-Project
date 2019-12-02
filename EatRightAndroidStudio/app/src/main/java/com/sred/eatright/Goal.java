7527d6fc7d0961ca5cba7376d857a27e9e19e205
HELLCAT25 <michael_z97@hotmail.com>
2019-12-01 18:07:31 -0600

    Update Goal.java

--- EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Goal.java
+++ EatRightAndroidStudio/app/src/main/java/com/sred/eatright/Goal.java
@@ -33,35 +33,35 @@ public class Goal {
 	}
 	
 	
-	public static class GoalBuilder {
-		private double curWeight;
-		private double goalWeight;
-		private FitnessGoal fitnessGoal;
-		private ActivityLevel activityLevel;
-		private NutritionGoal nutritionGoal;
-		
-		public GoalBuilder setCurWeight(double curWeight) {
-			this.curWeight = curWeight;
-			return this;
-		}
-		public GoalBuilder setGoalWeight(double goalWeight) {
-			this.goalWeight = goalWeight;
-			return this;
-		}
-		public GoalBuilder setFitnessGoal(FitnessGoal fitnessGoal) {
-			this.fitnessGoal = fitnessGoal;
-			return this;
-		}
-		public GoalBuilder setActivityLevel(ActivityLevel activityLevel) {
-			this.activityLevel = activityLevel;
-			return this;
-		}
-		public GoalBuilder setNutritionGoal(NutritionGoal nutritionGoal) {
-			this.nutritionGoal = nutritionGoal;
-			return this;
-		}
-		public Goal buildGoal() {
-			return new Goal(this);
-		}
-	}
+//	public static class GoalBuilder {
+//		private double curWeight;
+//		private double goalWeight;
+//		private FitnessGoal fitnessGoal;
+//		private ActivityLevel activityLevel;
+//		private NutritionGoal nutritionGoal;
+//
+//		public GoalBuilder setCurWeight(double curWeight) {
+//			this.curWeight = curWeight;
+//			return this;
+//		}
+//		public GoalBuilder setGoalWeight(double goalWeight) {
+//			this.goalWeight = goalWeight;
+//			return this;
+//		}
+//		public GoalBuilder setFitnessGoal(FitnessGoal fitnessGoal) {
+//			this.fitnessGoal = fitnessGoal;
+//			return this;
+//		}
+//		public GoalBuilder setActivityLevel(ActivityLevel activityLevel) {
+//			this.activityLevel = activityLevel;
+//			return this;
+//		}
+//		public GoalBuilder setNutritionGoal(NutritionGoal nutritionGoal) {
+//			this.nutritionGoal = nutritionGoal;
+//			return this;
+//		}
+//		public Goal buildGoal() {
+//			return new Goal(this);
+//		}
+//	}
 }
